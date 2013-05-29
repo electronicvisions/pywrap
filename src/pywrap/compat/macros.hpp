17ea8de3e08c36baa065f4bc6dbf34bd5d034e4a
Eric Müller <mueller@kip.uni-heidelberg.de>
2013-05-29 18:21:09 +0200

    add pywrap py++ macro PYPP_UNI_INIT for () -> {} uniform initialization

--- src/pywrap/compat/macros.hpp
+++ src/pywrap/compat/macros.hpp
@@ -10,6 +10,7 @@
 #define PYPP_EXPLICIT_CAST
 #define PYPP_INSTANTIATE(TYPE) inline void _instantiate(TYPE& a) { static_cast<void>(a); }
 #define PYPP_DEFAULT(x)
+#define PYPP_UNI_INIT(x) ( x )
 #else
 #define PYPP_CONSTEXPR constexpr
 #define PYPP_TYPED_ENUM(name, type) enum name : type
@@ -17,6 +18,7 @@
 #define PYPP_EXPLICIT_CAST explicit
 #define PYPP_INSTANTIATE(TYPE)
 #define PYPP_DEFAULT(x) x = default
+#define PYPP_UNI_INIT(x) { x }
 #endif
 
 #if __cplusplus >= 201103L
