a095cbf3e08589d31d484d35e25a8a1c5c8d7f92
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2013-07-03 13:50:32 +0200

    rm PYPP_UNI_INIT because it's completely broken

--- src/pywrap/compat/macros.hpp
+++ src/pywrap/compat/macros.hpp
@@ -11,7 +11,6 @@
 #define PYPP_INSTANTIATE(TYPE) inline void _instantiate(TYPE& a) { static_cast<void>(a); }
 #define PYPP_DEFAULT(x) x {}
 #define PYPP_DELETE(x)
-#define PYPP_UNI_INIT(x) ( x )
 #else
 #define PYPP_CONSTEXPR constexpr
 #define PYPP_TYPED_ENUM(name, type) enum name : type
@@ -20,7 +19,6 @@
 #define PYPP_INSTANTIATE(TYPE)
 #define PYPP_DEFAULT(x) x = default
 #define PYPP_DELETE(x) x = delete
-#define PYPP_UNI_INIT(x) { x }
 #endif
 
 #if __cplusplus >= 201103L
