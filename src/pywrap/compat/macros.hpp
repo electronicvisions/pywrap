1c1531b777b45f926ea9b40af579e8615dd873ab
Bernhard Vogginger <Bernhard.Vogginger@tu-dresden.de>
2015-07-22 14:51:38 +0200

    macro for typed class enum
    Change-Id: I192d5aeef84b2ba76cc246c87243014168f5b211

--- src/pywrap/compat/macros.hpp
+++ src/pywrap/compat/macros.hpp
@@ -6,6 +6,7 @@
 #define nullptr NULL
 #define PYPP_TYPED_ENUM(name, type) enum name
 #define PYPP_CLASS_ENUM(name) enum name
+#define PYPP_TYPED_CLASS_ENUM(name, type) enum name
 #define PYPP_TYPENAME /* this is only due to gcc4.4 ignoring the c++ std */
 #define PYPP_EXPLICIT_CAST
 #define PYPP_INSTANTIATE(TYPE) static inline void pywrap_instantiate(TYPE& a) { static_cast<void>(a); }
@@ -16,6 +17,7 @@
 #define PYPP_CONSTEXPR constexpr
 #define PYPP_TYPED_ENUM(name, type) enum name : type
 #define PYPP_CLASS_ENUM(name) enum class name
+#define PYPP_TYPED_CLASS_ENUM(name, type) enum class name : type
 #define PYPP_TYPENAME typename
 #define PYPP_EXPLICIT_CAST explicit
 #define PYPP_INSTANTIATE(TYPE) static inline void pywrap_instantiate(TYPE& a) { static_cast<void>(a); }
